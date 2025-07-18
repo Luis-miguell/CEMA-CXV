5a5d211063c2b6c099119f1ec47714e67ea54101
Josh Vanegas <vanegasmejiajosepharmando3@gmail.com>
2025-07-18 18:34:51 -0500

    Correccion de app.jsx line 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 import FormLogin from "./components/Login";
 import Landing from "./landing_page/landing";
 
-const [usuario, setUsuario] = useState({nombre: "Admin", email: "admin@example.com", contrasenia: "12345"});
+
 
 
 function App() {
+    const [usuario] = useState({nombre: "Admin", email: "admin@example.com", contrasenia: "12345"});
 
     return (
       <>
