5b38d0a28c8b9e716023da027c55bb40d80fb28e
Luis Miguel <miguepach100@gmail.com>
2025-08-01 16:31:26 -0500

    Sugerencias en las clases

--- src/servicio/UserService.js
+++ src/servicio/UserService.js
@@ -40,3 +40,131 @@ class UserService {
 }
 
 export default UserService;
+
+
+
+/* import User from "../modelo/User.js";
+class GestionUser{
+    constructor(base = "users"){
+        this.base = base;
+        this.usuarios = JSON.parse(localStorage.getItem(base)) || [];
+    }
+
+    getAllUsers(){
+        return this.usuarios.map(user => new User(user));
+    }
+    
+    getUser(id){
+        let user = this.usuarios.find(user => user.id === id);
+        if(!user){
+            alert("Usuario no encontrado")
+            return null
+        }
+        return new User(user);
+    }
+
+    userExist(email){
+        return this.usuarios.some(user => user.email === email);
+    }
+
+    addUser(info){
+        if(this.userExist(info.email)){
+            alert("El correo electronico ya está asociado a un cuenta existente.");
+        } else {
+            this.usuarios.push(new User(info));
+            this.saveUsers();
+        }
+    }
+
+    deleteUser(id){
+        this.usuarios = this.usuarios.filter(user => user.id !== id);
+        this.saveUsers();
+    }
+
+    saveUsers(){
+        localStorage.setItem(this.base, JSON.stringify(this.usuarios));
+    }
+
+    deleteUsers(){
+        let confirmacion = confirm("¿Seguro que quieres borrar todos los usuarios? \nEsta acción no puede revertirse");
+        if(confirmacion){
+            this.usuarios = [];
+            this.saveUsers();
+        }
+    }
+
+    editUser(id, newDates){
+        let idx = timport User from "../modelo/User.js";
+class GestionUser{
+    constructor(base = "users"){
+        this.base = base;
+        this.usuarios = JSON.parse(localStorage.getItem(base)) || [];
+    }
+
+    getAllUsers(){
+        return this.usuarios.map(user => new User(user));
+    }
+    
+    getUser(id){
+        let user = this.usuarios.find(user => user.id === id);
+        if(!user){
+            alert("Usuario no encontrado")
+            return null
+        }
+        return new User(user);
+    }
+
+    userExist(email){
+        return this.usuarios.some(user => user.email === email);
+    }
+
+    addUser(info){
+        if(this.userExist(info.email)){
+            alert("El correo electronico ya está asociado a un cuenta existente.");
+        } else {
+            this.usuarios.push(new User(info));
+            this.saveUsers();
+        }
+    }
+
+    deleteUser(id){
+        this.usuarios = this.usuarios.filter(user => user.id !== id);
+        this.saveUsers();
+    }
+
+    saveUsers(){
+        localStorage.setItem(this.base, JSON.stringify(this.usuarios));
+    }
+
+    deleteUsers(){
+        let confirmacion = confirm("¿Seguro que quieres borrar todos los usuarios? \nEsta acción no puede revertirse");
+        if(confirmacion){
+            this.usuarios = [];
+            this.saveUsers();
+        }
+    }
+
+    editUser(id, newDates){
+        let idx = this.usuarios.findIndex(user => user.id === id);
+        if(idx === -1 ) alert("Usuario no encontrado");
+        this.usuarios[idx] = {
+            ...this.usuarios[idx],
+            ...newDates,
+            id: this.usuarios[idx].id
+        };
+        this.saveUsers();
+        return new User(this.usuarios[idx])
+    }
+
+}his.usuarios.findIndex(user => user.id === id);
+        if(idx === -1 ) alert("Usuario no encontrado");
+        this.usuarios[idx] = {
+            ...this.usuarios[idx],
+            ...newDates,
+            id: this.usuarios[idx].id
+        };
+        this.saveUsers();
+        return new User(this.usuarios[idx])
+    }
+
+} */
