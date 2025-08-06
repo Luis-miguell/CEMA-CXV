8cf704921ca7574638a8b2ca0188e5edcb6e97a8
Josh Vanegas <vanegasmejiajosepharmando3@gmail.com>
2025-08-06 18:05:28 -0500

    Register w some bugs

--- src/UserProvider.jsx
+++ src/UserProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import User from "../modelo/User.js";
+import User from "./modelo/User";
 
 const UserContext = createContext();
 
--- src/vista/components/register/register.jsx
+++ src/vista/components/register/register.jsx
@@ -1,12 +1,30 @@
 import '../landing_page/landing.css';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+import { useContext } from "react";
+import { UserContext } from "../../../UserProvider.jsx";
+import User from "../../../modelo/User.js";
+/* import { useNavigate } from 'react-router-dom'; */
 
 function Register() {
 
+  /* const Navigate = useNavigate(); */
+
+  const { usuario, setUsuario } = useContext(UserContext);
+  let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
   const handleRegister = async (e) => {
+    
    
     e.preventDefault();
-
+    if (!usuario.name || !usuario.lastname || !usuario.email || !usuario.getPassword()) {
+      alert("Por favor, completa todos los campos.");
+      return;
+    }
+    usuarios.push(usuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    alert("Registro exitoso!");
+    setUsuario(new User(usuario)); 
+    Navigate("/login"); 
   }
   
   return (
@@ -21,7 +39,14 @@ function Register() {
       type="text" 
       id="name" 
       placeholder="Tu nombre;)"
-      
+      onInput={e => {
+        setUsuario(user => ({
+          ...user,
+          name: e.target.value
+        }))
+      }}
+      value={usuario.name}
+      required
       />
       
       <label htmlFor="apellido">Apellido</label>
@@ -29,20 +54,39 @@ function Register() {
       type="text" 
       id="apellido" 
       placeholder="Tu apellido;)"
+      onInput={e => {
+        setUsuario(user => ({
+          ...user,
+          apellido: e.target.value
+        }))
+      }}
+      value={usuario.apellido}
+      required
       />
       <label htmlFor="email">Email</label>
       <input 
       type="email" 
       id="email" 
       placeholder="Tu email ;)"
-      
+      onInput={e => {
+        setUsuario(user => ({
+          ...user,
+          email: e.target.value
+        }))
+      }}
+      value={usuario.email}
+      required
       />
       <label htmlFor="password">Contraseña</label>
       <input 
       type="password" 
       id="password" 
       placeholder="Crea una contraseña. Segura y fácil de recordar;)"
-      
+      onInput={e => {
+        setUsuario(user => user.setPassword(e.target.value))
+      }}
+      value={usuario.getPassword()}
+      required
       />
       <div className="register-actions">
         <button type='submit'>Registrarse</button>
